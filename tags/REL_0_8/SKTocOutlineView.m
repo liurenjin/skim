4cddcba800123c3db8c6547cf1aab5afca05f350
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2007-09-03 09:27:10 +0000

    Tag for release

---
+++ tags/REL_0_8/SKTocOutlineView.m
@@ -0,0 +1,180 @@
+//
+//  SKTocOutlineView.m
+//  Skim
+//
+//  Created by Christiaan Hofman on 2/25/07.
+/*
+ This software is Copyright (c) 2007
+ Christiaan Hofman. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+ - Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+ - Neither the name of Christiaan Hofman nor the names of any
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "SKTocOutlineView.h"
+#import "SKTypeSelectHelper.h"
+
+
+@implementation SKTocOutlineView
+
+- (void)dealloc {
+    if (trackingRects != NULL)
+        CFRelease(trackingRects);
+    [super dealloc];
+}
+
+- (void)highlightSelectionInClipRect:(NSRect)clipRect {
+    if ([[self delegate] respondsToSelector:@selector(outlineViewHighlightedRows:)]) {
+        NSMutableIndexSet *rowIndexes = [[[self selectedRowIndexes] mutableCopy] autorelease];
+        NSArray *rows = [[self delegate] outlineViewHighlightedRows:self];
+        NSColor *color = ([[self window] isKeyWindow] && [[self window] firstResponder] == self) ? [NSColor alternateSelectedControlColor] : [NSColor secondarySelectedControlColor];
+        float factor = 0.5;
+        int i, count = [rows count];
+        
+        [NSGraphicsContext saveGraphicsState];
+        for (i = 0; i < count; i++) {
+            int row = [[rows objectAtIndex:i] intValue];
+            [[[NSColor controlBackgroundColor] blendedColorWithFraction:factor ofColor:color] set];
+            factor -= 0.1;
+            if ([rowIndexes containsIndex:row] == NO) {
+                NSRectFill([self rectOfRow:row]);
+                [rowIndexes addIndex:row];
+            }
+            if (factor <= 0.0) break;
+        }
+        [NSGraphicsContext restoreGraphicsState];
+    }
+    [super highlightSelectionInClipRect:clipRect]; 
+}
+
+- (void)removeTrackingRects {
+    if (trackingRects) {
+        CFIndex idx = CFArrayGetCount(trackingRects);
+        while(idx--)
+            [self removeTrackingRect:(NSTrackingRectTag)CFArrayGetValueAtIndex(trackingRects, idx)];
+        CFArrayRemoveAllValues(trackingRects);
+    }
+}
+
+- (void)rebuildTrackingRects {
+    if ([[self delegate] respondsToSelector:@selector(outlineView:shouldTrackTableColumn:item:)] == NO)
+        return;
+    
+    if (trackingRects == nil)
+        trackingRects = CFArrayCreateMutable(kCFAllocatorDefault, 0, NULL);
+    else
+        [self removeTrackingRects];
+    
+    NSRange rowRange = [self rowsInRect:[self visibleRect]];
+    NSRange columnRange = [self columnsInRect:[self visibleRect]];
+    unsigned int row, column;
+	NSTableColumn *tableColumn;
+    int userData;
+    NSTrackingRectTag tag;
+    
+    for (column = columnRange.location; column < NSMaxRange(columnRange); column++) {
+        tableColumn = [[self tableColumns] objectAtIndex:column];
+		for (row = rowRange.location; row < NSMaxRange(rowRange); row++) {
+            if ([[self delegate] outlineView:self shouldTrackTableColumn:tableColumn item:[self itemAtRow:row]]) {
+                userData = row * [self numberOfColumns] + column;
+                tag = [self addTrackingRect:[self frameOfCellAtColumn:column row:row] owner:self userData:(void *)userData assumeInside:NO];
+                CFArrayAppendValue(trackingRects, (const void *)tag);
+            }
+        }
+    }
+}
+
+- (void)expandItem:(id)item expandChildren:(BOOL)collapseChildren {
+    // NSOutlineView does not call resetCursorRect when expanding
+    [super expandItem:item expandChildren:collapseChildren];
+	[self rebuildTrackingRects];
+}
+
+- (void)collapseItem:(id)item collapseChildren:(BOOL)collapseChildren {
+    // NSOutlineView does not call resetCursorRect when collapsing
+    [super collapseItem:item collapseChildren:collapseChildren];
+	[self rebuildTrackingRects];
+}
+
+- (void)reloadData{
+    [super reloadData];
+	[self rebuildTrackingRects];
+}
+
+- (void)resetCursorRects {
+	[super resetCursorRects];
+    [self rebuildTrackingRects];
+}
+
+- (void)setDataSource:(id)anObject {
+	[super setDataSource:anObject];
+	[self rebuildTrackingRects];
+}
+
+- (void)noteNumberOfRowsChanged {
+	[super noteNumberOfRowsChanged];
+	[self rebuildTrackingRects];
+}
+
+- (void)viewWillMoveToWindow:(NSWindow *)newWindow {
+    if ([self window])
+        [self removeTrackingRects];
+}
+
+- (void)viewDidMoveToWindow {
+	if ([self window])
+        [self rebuildTrackingRects];
+}
+
+- (void)mouseEntered:(NSEvent *)theEvent{
+    if ([[self delegate] respondsToSelector:@selector(outlineView:mouseEnteredTableColumn:item:)]) {
+        int userData = (int)[theEvent userData];
+        int numCols = [self numberOfColumns];
+		int column = userData % numCols;
+		int row = userData / numCols;
+        if (column != -1 && row != -1) {
+            NSTableColumn *tableColumn = [[self tableColumns] objectAtIndex:column];
+            [[self delegate] outlineView:self mouseEnteredTableColumn:tableColumn item:[self itemAtRow:row]];
+        }
+	}
+}
+
+- (void)mouseExited:(NSEvent *)theEvent{
+    if ([[self delegate] respondsToSelector:@selector(outlineView:mouseExitedTableColumn:item:)]) {
+        int userData = (int)[theEvent userData];
+        int numCols = [self numberOfColumns];
+		int column = userData % numCols;
+		int row = userData / numCols;
+        if (column != -1 && row != -1) {
+            NSTableColumn *tableColumn = [[self tableColumns] objectAtIndex:column];
+            [[self delegate] outlineView:self mouseExitedTableColumn:tableColumn item:[self itemAtRow:row]];
+        }
+	}
+}
+
+@end
---
+++ tags/REL_0_8/SKTypeSelectHelper.m
@@ -0,0 +1,311 @@
+//
+//  SKTypeSelectHelper.m
+//  Skim
+//
+//  Created by Christiaan Hofman on 8/21/07.
+/*
+ This software is Copyright (c) 2007
+ Christiaan Hofman. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 
+ - Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 
+ - Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in
+ the documentation and/or other materials provided with the
+ distribution.
+ 
+ - Neither the name of Christiaan Hofman nor the names of any
+ contributors may be used to endorse or promote products derived
+ from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "SKTypeSelectHelper.h"
+
+#define REPEAT_CHARACTER '/'
+
+@interface NSString (BDSKTypeAheadHelperExtensions)
+- (BOOL)containsStringStartingAtWord:(NSString *)string options:(int)mask range:(NSRange)range;
+@end
+
+#pragma mark -
+
+@interface SKTypeSelectHelper (SKPrivate)
+- (NSTimeInterval)timeoutInterval;
+- (NSArray *)searchCache;
+- (void)searchWithStickyMatch:(BOOL)allowUpdate;
+- (void)stopTimer;
+- (void)startTimerForSelector:(SEL)selector;
+- (void)typeSelectSearchTimeout:(NSTimer *)aTimer;
+- (void)typeSelectCleanTimeout:(NSTimer *)aTimer;
+- (unsigned int)indexOfMatchedItemAfterIndex:(unsigned int)selectedIndex;
+@end
+
+@implementation SKTypeSelectHelper
+
+// Init and dealloc
+
+- (id)init {
+    if (self = [super init]){
+        searchString = [[NSMutableString alloc] init];
+        cycleResults = YES;
+        matchesImmediately = YES;
+        matchOption = SKPrefixMatch;
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [self stopTimer];
+    [searchString release];
+    [searchCache release];
+    [super dealloc];
+}
+
+#pragma mark Accessors
+
+- (id)dataSource {
+    return dataSource;
+}
+
+- (void)setDataSource:(id)newDataSource {
+    if (dataSource != newDataSource) {
+        dataSource = newDataSource;
+        [self rebuildTypeSelectSearchCache];
+    }
+}
+
+- (BOOL)cyclesSimilarResults {
+    return cycleResults;
+}
+
+- (void)setCyclesSimilarResults:(BOOL)newValue {
+    cycleResults = newValue;
+}
+
+- (BOOL)matchesImmediately {
+    return matchesImmediately;
+}
+
+- (void)setMatchesImmediately:(BOOL)newValue {
+    matchesImmediately = newValue;
+}
+
+- (int)matchOption {
+    return matchOption;
+}
+
+- (void)setMatchOption:(int)newValue {
+    matchOption = newValue;
+}
+
+- (BOOL)isProcessing {
+    return processing;
+}
+
+#pragma mark API
+
+- (void)rebuildTypeSelectSearchCache {    
+    if (searchCache)
+        [searchCache release];
+    
+    searchCache = [[dataSource typeSelectHelperSelectionItems:self] retain];
+}
+
+- (void)processKeyDownCharacter:(unichar)character {
+    if (processing == NO)
+        [searchString setString:@""];
+    
+    // Append the new character to the search string
+    [searchString appendFormat:@"%C", character];
+    
+    if ([dataSource respondsToSelector:@selector(typeSelectHelper:updateSearchString:)])
+        [dataSource typeSelectHelper:self updateSearchString:searchString];
+    
+    // Reset the timer if it hasn't expired yet
+    [self startTimerForSelector:@selector(typeSelectSearchTimeout:)];
+    
+    if (matchesImmediately)
+        [self searchWithStickyMatch:processing];
+    
+    processing = YES;
+}
+
+- (void)repeatSearch {
+    [self searchWithStickyMatch:NO];
+    
+    if ([searchString length] && [dataSource respondsToSelector:@selector(typeSelectHelper:updateSearchString:)])
+        [dataSource typeSelectHelper:self updateSearchString:searchString];
+    
+    [self startTimerForSelector:@selector(typeSelectCleanTimeout:)];
+    
+    processing = NO;
+}
+
+- (BOOL)isTypeSelectCharacter:(unichar)character {
+    if ([[NSCharacterSet alphanumericCharacterSet] characterIsMember:character])
+        return YES;
+    if ([self isProcessing] && [[NSCharacterSet controlCharacterSet] characterIsMember:character] == NO)
+        return YES;
+    return NO;
+}
+
+- (BOOL)isRepeatCharacter:(unichar)character {
+    return [self cyclesSimilarResults] && character == REPEAT_CHARACTER;
+}
+
+@end 
+
+#pragma mark -
+
+@implementation SKTypeSelectHelper (SKPrivate)
+
+// See http://www.mactech.com/articles/mactech/Vol.18/18.10/1810TableTechniques/index.html
+- (NSTimeInterval)timeoutInterval {
+    int keyThreshTicks = [[NSUserDefaults standardUserDefaults] integerForKey:@"InitialKeyRepeat"];
+    if (0 == keyThreshTicks)
+        keyThreshTicks = 35;	// apparent default value, translates to 1.17 sec timeout.
+    
+    return fmin(2.0 / 60.0 * keyThreshTicks, 2.0);
+}
+
+- (NSArray *)searchCache {
+    if (searchCache == nil)
+        [self rebuildTypeSelectSearchCache];
+    return searchCache;
+}
+
+- (void)stopTimer {
+    [timer invalidate];
+    [timer release];
+    timer = nil;
+}
+
+- (void)startTimerForSelector:(SEL)selector {
+    [self stopTimer];
+    timer = [[NSTimer alloc] initWithFireDate:[NSDate dateWithTimeIntervalSinceNow:[self timeoutInterval]] interval:0 target:self selector:selector userInfo:NULL repeats:NO];
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
+}
+
+- (void)typeSelectSearchTimeout:(NSTimer *)aTimer {
+    if (matchesImmediately == NO)
+        [self searchWithStickyMatch:NO];
+    [self typeSelectCleanTimeout:aTimer];
+}
+
+- (void)typeSelectCleanTimeout:(NSTimer *)aTimer {
+    if ([dataSource respondsToSelector:@selector(typeSelectHelper:updateSearchString:)])
+        [dataSource typeSelectHelper:self updateSearchString:nil];
+    [self stopTimer];
+    processing = NO;
+}
+
+- (void)searchWithStickyMatch:(BOOL)sticky {
+    if ([searchString length]) {
+        unsigned int selectedIndex, startIndex, foundIndex;
+        
+        if (cycleResults) {
+            selectedIndex = [dataSource typeSelectHelperCurrentlySelectedIndex:self];
+            if (selectedIndex >= [[self searchCache] count])
+                selectedIndex = NSNotFound;
+        } else {
+            selectedIndex = NSNotFound;
+        }
+        
+        startIndex = selectedIndex;
+        if (sticky && selectedIndex != NSNotFound)
+            startIndex = startIndex > 0 ? startIndex - 1 : [[self searchCache] count] - 1;
+        
+        foundIndex = [self indexOfMatchedItemAfterIndex:startIndex];
+        
+        // Avoid flashing a selection all over the place while you're still typing the thing you have selected
+        if (foundIndex != NSNotFound && foundIndex != selectedIndex)
+            [dataSource typeSelectHelper:self selectItemAtIndex:foundIndex];
+    }
+}
+
+- (unsigned int)indexOfMatchedItemAfterIndex:(unsigned int)selectedIndex {
+    unsigned int labelCount = [[self searchCache] count];
+    
+    if (labelCount == NO)
+        return NSNotFound;
+    
+    if (selectedIndex == NSNotFound)
+        selectedIndex = labelCount - 1;
+
+    unsigned int labelIndex = selectedIndex;
+    BOOL looped = NO;
+    int options = NSCaseInsensitiveSearch;
+    
+    if (matchOption == SKPrefixMatch)
+        options |= NSAnchoredSearch;
+    
+    while (looped == NO) {
+        NSString *label;
+        
+        if (++labelIndex == labelCount)
+            labelIndex = 0;
+        if (labelIndex == selectedIndex)
+            looped = YES;
+        
+        label = [[self searchCache] objectAtIndex:labelIndex];
+        
+        if (matchOption == SKFullStringMatch) {
+            if ([label caseInsensitiveCompare:searchString] == NSOrderedSame)
+                return labelIndex;
+        } else {
+            if ([label containsStringStartingAtWord:searchString options:options range:NSMakeRange(0, [label length])]) {
+                return labelIndex;
+            }
+        }
+    }
+    
+    return NSNotFound;
+}
+
+@end
+
+#pragma mark -
+
+@implementation NSString (BDSKTypeAheadHelperExtensions)
+
+- (BOOL)containsStringStartingAtWord:(NSString *)string options:(int)mask range:(NSRange)range {
+    unsigned int stringLength = [string length];
+    if (stringLength == 0 || stringLength > range.length)
+        return NO;
+    while (range.length >= stringLength) {
+        NSRange r = [self rangeOfString:string options:mask range:range];
+        if (r.location == NSNotFound)
+            return NO;
+        // see if we start at a "word boundary"
+        if (r.location == 0 || [[NSCharacterSet alphanumericCharacterSet] characterIsMember:[self characterAtIndex:r.location - 1]] == NO)
+            return YES;
+        // if it's anchored, we only should search once
+        if (mask & NSAnchoredSearch)
+            return NO;
+        // get the new range, shifted by one from the last match
+        if (mask & NSBackwardsSearch)
+            range = NSMakeRange(range.location, NSMaxRange(r) - range.location - 1);
+        else
+            range = NSMakeRange(r.location + 1, NSMaxRange(range) - r.location - 1);
+    }
+    return NO;
+}
+
+@end
